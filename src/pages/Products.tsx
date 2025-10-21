ee381ba2932581a8c881245b336e3e493c770e5a
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-10-21 09:14:00 -0300

    feat: implementing nav home to products and products to home

---
+++ src/pages/Products.tsx
@@ -0,0 +1,12 @@
+export function Products(){
+    return(
+        <div>
+            <nav>
+                <a href="/">Voltar</a>
+            </nav>
+
+            <h1>Produtos</h1>
+        </div>
+
+    )
+}
