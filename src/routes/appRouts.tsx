ac4c91baf37fc442441e1fbc4a3817b83e47ce96
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-11-14 10:06:20 -0300

    feature: implementing 404 page in react navegation

--- src/routes/appRouts.tsx
+++ src/routes/appRouts.tsx
@@ -1,13 +1,21 @@
 import {Routes, Route} from 'react-router'
 
-import { Home } from '../pages/home'
+import { Home } from '../pages/Home'
 import { Products } from '../pages/Products'
+import { NotFound } from "../pages/NotFound"
 
 export function AppRoutes(){
     return(
         <Routes>
             <Route path='/' index element={<Home/>} />
             <Route path='/products' element={<Products/>} />
+
+            <Route path='*' element={ <NotFound /> } />
         </Routes>
     )
 }
+
+/*  <Route path='*' element={ <NotFound /> } /> 
+
+Sempre está por último, passou por todo mundo então vai para ela
+*/
